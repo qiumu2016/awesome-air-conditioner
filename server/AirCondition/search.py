b95bb0387caf868379270b1eb004de38551478c7
FishClover <847915793@qq.com>
2019-05-26 19:17:15 +0800

    一些修改

--- server/AirCondition/search.py
+++ server/AirCondition/search.py
@@ -1,6 +1,16 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render_to_response
+from conditioner import conditioner
 
-def power_on (self, request):
+def powerOn (self, request):
     response = {}
     return JsonResponse(response)
+
+def startUp(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def setPara(self,request):
+    response = {}
+    return JsonResponse(response)
+
