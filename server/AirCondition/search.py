cd35e3e6f2f50e8a7b7a6e74f106d0b463f8f6e8
sty410 <783824288@qq.com>
2019-05-26 19:39:08 +0800

    update caozuoqiyue

--- server/AirCondition/search.py
+++ server/AirCondition/search.py
@@ -2,11 +2,43 @@ from django.http.response import JsonResponse
 from django.shortcuts import render_to_response
 from conditioner import conditioner
 
-def powerOn (self, request):
+def requestOn(self,request):
     response = {}
     return JsonResponse(response)
 
-def startUp(self,request):
+def changeTargetTemp(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def changeFanSpeed(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def requestOff(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def requestFee(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def createRDR(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def printRDR(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def createInvoice(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def printInvoice(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def powerOn (self, request):
     response = {}
     return JsonResponse(response)
 
@@ -14,3 +46,18 @@ def setPara(self,request):
     response = {}
     return JsonResponse(response)
 
+def startUp(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def checkRoomState(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def queryReport(self,request):
+    response = {}
+    return JsonResponse(response)
+
+def printReport(self,request):
+    response = {}
+    return JsonResponse(response)
