bf6036f3505e0a0a3e0bf44c57b0b25a2a945b20
FishClover <847915793@qq.com>
2019-05-26 14:40:27 +0800

    修改

--- server/AirCondition/search.py
+++ server/AirCondition/search.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http.response import JsonResponse
 from django.shortcuts import render_to_response
 
-def power_on (request):
-    return HttpResponse("12")
+def power_on (self, request):
+    response = {}
+    return JsonResponse(response)
