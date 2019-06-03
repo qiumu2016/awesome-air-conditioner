e623a6ef856165b8e176caf359486b5cc49062d7
sty410 <783824288@qq.com>
2019-06-04 00:15:18 +0800

    修改查询不到updateid的判定条件

--- server/AirCondition/search.py
+++ server/AirCondition/search.py
@@ -232,7 +232,7 @@ def changeFanSpeed(request): #顾客更改空调风速
             t2 = datetime.datetime.now()
             updateId = cursor.fetchone()
             updateIdStr = str(updateId)
-            if (updateId != '[]'):
+            if (updateIdStr != '[]'):
                 updateIdStr = updateIdStr[1:-2]
                 updateDetailSql1 = '''update AirCondition_details
                                       set end_time = ?, end_temp = ?, fee = ?
